d81c8118d640f0949e7f2775f1259c409f1dab6b
alighazal <ali.mdghazal@gmail.com>
2021-01-15 07:48:49 +0200

    feature:working ugly chat it still needs styling .. most of the functionality is done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const {addUser, removeUser, getUsers, getUsersInRoom} = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -20,19 +22,48 @@ const io = socketio(server,{
 
 io.on('connection', (socket) => {
     
-    console.log("New Connection!!!");
+    console.log(`New Connection!!! ${socket.id}`);
  
     socket.on("join", ({name,room}, callback) => {
-        console.log(name, room);
-        const error = true;
 
-        if (error) {
-            callback({error: 'error'})
-        }
+       const {error, user} = addUser({id: socket.id, name, room});
+
+       if (error) 
+            return callback(error);    
+
+        socket.join(user.room);
+
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to room ${user.room} `});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined the room `});
+
+        socket.emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        socket.broadcast.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        
+
+        
+        
+        callback(); 
+
+    });
+
+
+    socket.on('sendMessage', (message, callback) => {
+
+        const user = getUsers(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
     });
     
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        const user = removeUser(socket.id);
+        removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+          }
     });
     
 })
